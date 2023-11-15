ef4f149d00c2663d891abba40c6f2efba934a338
vvb2060 <vvb2060@gmail.com>
2023-11-15 18:19:10 +0800

    Add AOSP RSA public key

--- app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
+++ app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
@@ -42,12 +42,19 @@ public class CertificateInfo {
             "ixPvZtXQpUpuL12ab+9EaDK8Z4RHJYYfCT3Q5vNAXaiWQ+8PTWm2QgBR/bkwSWc+" +
             "NpUFgNPN9PvQi8WEg5UmAGMCAwEAAQ==";
 
-    private static final String AOSP_ROOT_PUBLIC_KEY = "" +
+    private static final String AOSP_ROOT_EC_PUBLIC_KEY = "" +
             "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE7l1ex+HA220Dpn7mthvsTWpdamgu" +
             "D/9/SQ59dx9EIm29sa/6FsvHrcV30lacqrewLVQBXT5DKyqO107sSHVBpA==";
 
+    private static final String AOSP_ROOT_RSA_PUBLIC_KEY = "" +
+            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCia63rbi5EYe/VDoLmt5TRdSMf" +
+            "d5tjkWP/96r/C3JHTsAsQ+wzfNes7UA+jCigZtX3hwszl94OuE4TQKuvpSe/lWmg" +
+            "MdsGUmX4RFlXYfC78hdLt0GAZMAoDo9Sd47b0ke2RekZyOmLw9vCkT/X11DEHTVm" +
+            "+Vfkl5YLCazOkjWFmwIDAQAB";
+
     private static final byte[] googleKey = Base64.decode(GOOGLE_ROOT_PUBLIC_KEY, 0);
-    private static final byte[] aospKey = Base64.decode(AOSP_ROOT_PUBLIC_KEY, 0);
+    private static final byte[] aospEcKey = Base64.decode(AOSP_ROOT_EC_PUBLIC_KEY, 0);
+    private static final byte[] aospRsaKey = Base64.decode(AOSP_ROOT_RSA_PUBLIC_KEY, 0);
     private static JsonObject revocationJson;
 
     private final X509Certificate cert;
@@ -89,7 +96,9 @@ public class CertificateInfo {
         var publicKey = cert.getPublicKey().getEncoded();
         if (Arrays.equals(publicKey, googleKey)) {
             issuer = KEY_GOOGLE;
-        } else if (Arrays.equals(publicKey, aospKey)) {
+        } else if (Arrays.equals(publicKey, aospEcKey)) {
+            issuer = KEY_AOSP;
+        } else if (Arrays.equals(publicKey, aospRsaKey)) {
             issuer = KEY_AOSP;
         }
     }
