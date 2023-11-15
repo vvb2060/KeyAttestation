bb820681fbc92874b715dee2ed1f8f06e753f2c0
vvb2060 <vvb2060@gmail.com>
2023-11-16 03:01:00 +0800

    Set subject for app attest key
    PKCS7 does not maintain the order of certificates in a certification path. Set unique subject to reduce sorting issues.

--- app/src/main/java/io/github/vvb2060/keyattestation/home/HomeViewModel.kt
+++ app/src/main/java/io/github/vvb2060/keyattestation/home/HomeViewModel.kt
@@ -44,6 +44,7 @@ import java.security.cert.CertificateParsingException
 import java.security.cert.X509Certificate
 import java.security.spec.ECGenParameterSpec
 import java.util.Date
+import javax.security.auth.x500.X500Principal
 
 class HomeViewModel(pm: PackageManager, private val sp: SharedPreferences) : ViewModel() {
 
@@ -110,6 +111,9 @@ class HomeViewModel(pm: PackageManager, private val sp: SharedPreferences) : Vie
             }
             if (attestKeyAlias != null && !attestKey) {
                 builder.setAttestKeyAlias(attestKeyAlias)
+            }
+            if (attestKey) {
+                builder.setCertificateSubject(X500Principal("CN=App Attest Key"))
             }
         }
         val keyPairGenerator = KeyPairGenerator.getInstance(
