da1961da2973c65b2da3cb7f97efb1e9975d4a30
chiteroman <98092901+chiteroman@users.noreply.github.com>
2024-06-26 11:52:04 +0200

    Update banned certs + remove online certs parsing

--- app/src/main/java/io/github/vvb2060/keyattestation/attestation/RevocationList.java
+++ app/src/main/java/io/github/vvb2060/keyattestation/attestation/RevocationList.java
@@ -1,82 +1,60 @@
 package io.github.vvb2060.keyattestation.attestation;
 
-import android.os.Build;
+import android.util.Log;
 
-import org.json.JSONException;
+import androidx.annotation.NonNull;
+
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.util.Locale;
 
 import io.github.vvb2060.keyattestation.AppApplication;
 import io.github.vvb2060.keyattestation.R;
 
 public record RevocationList(String status, String reason) {
-    private static final JSONObject data = getStatus();
+    private static final JSONObject json;
 
-    private static String toString(InputStream input) throws IOException {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return new String(input.readAllBytes(), StandardCharsets.UTF_8);
-        } else {
-            var output = new ByteArrayOutputStream(8192);
-            var buffer = new byte[8192];
-            for (int length; (length = input.read(buffer)) != -1; ) {
-                output.write(buffer, 0, length);
-            }
-            return output.toString();
-        }
-    }
-
-    private static JSONObject parseStatus(InputStream inputStream) throws IOException {
+    static {
         try {
-            var statusListJson = new JSONObject(toString(inputStream));
-            return statusListJson.getJSONObject("entries");
-        } catch (JSONException e) {
-            throw new IOException(e);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            int i;
+            try (InputStream in = AppApplication.app.getResources().openRawResource(R.raw.status)) {
+                while ((i = in.read()) > -1) {
+                    byteArrayOutputStream.write(i);
+                }
+            }
+
+            Log.i(AppApplication.TAG, "JSON size: " + byteArrayOutputStream.size());
+
+            json = new JSONObject(byteArrayOutputStream.toString());
+
+        } catch (Throwable t) {
+            Log.e(AppApplication.TAG, "getStatus", t);
+            throw new RuntimeException(t);
         }
     }
 
-    private static JSONObject getStatus() {
-        var statusUrl = "https://android.googleapis.com/attestation/status";
-        var resName = "android:string/vendor_required_attestation_revocation_list_url";
-        var res = AppApplication.app.getResources();
-        // noinspection DiscouragedApi
-        var id = res.getIdentifier(resName, null, null);
-        if (id != 0) {
-            var url = res.getString(id);
-            if (!statusUrl.equals(url) && url.toLowerCase(Locale.ROOT).startsWith("https")) {
-                // no network permission, waiting for user report
-                throw new RuntimeException("unknown status url: " + url);
-            }
-        }
-        try (var input = res.openRawResource(R.raw.status)) {
-            return parseStatus(input);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to parse certificate revocation status", e);
-        }
-    }
 
     public static RevocationList get(BigInteger serialNumber) {
         String serialNumberString = serialNumber.toString(16).toLowerCase();
-        JSONObject revocationStatus;
+
         try {
-            revocationStatus = data.getJSONObject(serialNumberString);
-        } catch (JSONException e) {
-            return null;
+            JSONObject entries = json.getJSONObject("entries");
+
+            JSONObject revoke = entries.getJSONObject(serialNumberString);
+
+            return new RevocationList(revoke.getString("status"), revoke.getString("reason"));
+
+        } catch (Throwable ignored) {
         }
-        try {
-            var status = revocationStatus.getString("status");
-            var reason = revocationStatus.getString("reason");
-            return new RevocationList(status, reason);
-        } catch (JSONException e) {
-            return new RevocationList("", "");
-        }
+
+        return null;
     }
 
+    @NonNull
     @Override
     public String toString() {
         return "status is " + status + ", reason is " + reason;
